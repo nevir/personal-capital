e002420704e36fb03d76ecc2571a07444c032f7b
Ian MacLeod <ian@nevir.net>
2021-12-18 14:50:01 -0800

    Set up jest to collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   projects: ['<rootDir>/test/functional'],
+
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 }
