76dd25ca78c5bbe36bc2ba5b6d49ff5a1586911e
Ian MacLeod <ian@nevir.net>
2021-12-22 20:17:33 -0800

    Skip functional suite and allow VS Code to autorun

--- test/functional/api/login.ts
+++ test/functional/api/login.ts
@@ -7,64 +7,71 @@ import { configOrPrompt, prompt } from '../../support/functional/dialog'
 // https://github.com/facebook/jest/issues/9759
 jest.setTimeout(2 ** 31 - 1)
 
+
 describe('APIClient Login Flows', () => {
-  let realUsername: string
-  let realPassword: string
+  // Skip in VS Code
+  if (process.env.VSCODE_PID) {
+    it('is skipped when running within VS Code', () => { })
+  } else {
+    let realUsername: string
+    let realPassword: string
 
-  it('was provided the required configuration', async () => {
-    realUsername = await configOrPrompt('username', 'Email Address')
-    realPassword = await configOrPrompt('password', 'Password')
-  })
+    console.log(process.env)
 
-  describe('SMS Flow', () => {
-    const client = new APIClient(new CookieJar(), fetch)
-    let deviceName: string
+    it('was provided the required configuration', async () => {
+      realUsername = await configOrPrompt('username', 'Email Address')
+      realPassword = await configOrPrompt('password', 'Password')
+    })
 
-    it('identifies the user', async () => {
-      const response = await client.call('login/identifyUser', {
-        bindDevice: false,
-        redirectTo: '',
-        referrerId: '',
-        skipFirstUse: '',
-        skipLinkAccount: false,
-        username: realUsername,
+    describe('SMS Flow', () => {
+      const client = new APIClient(new CookieJar(), fetch)
+      let deviceName: string
+
+      it('identifies the user', async () => {
+        const response = await client.call('login/identifyUser', {
+          bindDevice: false,
+          redirectTo: '',
+          referrerId: '',
+          skipFirstUse: '',
+          skipLinkAccount: false,
+          username: realUsername,
+        })
+      })
+
+      it('requests a SMS code', async () => {
+        const response = await client.call('credential/challengeSms', {
+          bindDevice: false,
+          challengeMethod: 'OP',
+          challengeReason: 'DEVICE_AUTH',
+        })
+      })
+
+      it('authenticates via the received SMS code', async () => {
+        const response = await client.call('credential/authenticateSms', {
+          bindDevice: false,
+          challengeMethod: 'OP',
+          challengeReason: 'DEVICE_AUTH',
+          code: await prompt('SMS Code'),
+        })
+      })
+
+      it('logs in via password', async () => {
+        const response = await client.call('credential/authenticatePassword', {
+          bindDevice: false,
+          deviceName: 'Functional Test Suite (nevir/personal-capital)',
+          passwd: realPassword,
+          redirectTo: '',
+          referrerId: '',
+          skipFirstUse: '',
+          skipLinkAccount: false,
+          username: realUsername,
+        })
+      })
+
+      it('can read the current session', async () => {
+        const response = await client.call('login/querySession', {})
+        expect(response.spHeader.authLevel).toEqual('SESSION_AUTHENTICATED')
       })
     })
-
-    it('requests a SMS code', async () => {
-      const response = await client.call('credential/challengeSms', {
-        bindDevice: false,
-        challengeMethod: 'OP',
-        challengeReason: 'DEVICE_AUTH',
-      })
-    })
-
-    it('authenticates via the received SMS code', async () => {
-      const response = await client.call('credential/authenticateSms', {
-        bindDevice: false,
-        challengeMethod: 'OP',
-        challengeReason: 'DEVICE_AUTH',
-        code: await prompt('SMS Code'),
-      })
-    })
-
-    it('logs in via password', async () => {
-      const response = await client.call('credential/authenticatePassword', {
-        bindDevice: false,
-        deviceName: 'Functional Test Suite (nevir/personal-capital)',
-        passwd: realPassword,
-        redirectTo: '',
-        referrerId: '',
-        skipFirstUse: '',
-        skipLinkAccount: false,
-        username: realUsername,
-      })
-    })
-
-    it('can read the current session', async () => {
-      const response = await client.call('login/querySession', {})
-      expect(response.spHeader.authLevel).toEqual('SESSION_AUTHENTICATED')
-    })
-  })
-
+  }
 })
